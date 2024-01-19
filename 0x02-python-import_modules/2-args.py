b82cdc76b6f13fafdca29967738bc6501d2c473d
Marshall-IronSide <meliodas.martin@gmail.com>
2024-01-19 00:54:52 +0000

    0x02. Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-argc = len(argv)
-if argc < 2:
-    print("{} arguments.".format(argc - 1))
-else:
-    if argc == 2:
-        print("{} argument:".format(argc - 1))
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.".format())
+    elif len(sys.argv) == 2:
+        print("1 argument:".format())
     else:
-        print("{} arguments:".format(argc - 1))
-    for n in range(1, argc):
-        print("{}: {}".format(n, argv[n]))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+    if len(sys.argv) != 1:
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
